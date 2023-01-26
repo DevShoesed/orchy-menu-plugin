0fb25842d3c69630a73d370802f0bd599f7edbb9
Francesco Scarpato <f.scarpato@custom.it>
2023-01-26 09:09:34 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import {BehaviorSubject, Observable, ReplaySubject, shareReplay} from 'rxjs'
+import {MenuItem, MyEvent} from './types'
+
+export class OrchyMenuApi {
+    private readonly  REGISTER_MENU_EVENT_LABEL = 'registerMenu'
+    private readonly  UNREGISTER_MENU_EVENT_LABEL = 'unregisterMenu'
+    
+    private menuItems: BehaviorSubject<MenuItem[]> = new BehaviorSubject<MenuItem[]>([])
+    private _eventBus?:ReplaySubject<unknown>
+    
+
+    menuItems$ :Observable<MenuItem[]> = this.menuItems.asObservable().pipe(shareReplay(1))
+
+    constructor(eventBus:ReplaySubject<unknown>){
+        this._eventBus = eventBus
+        
+        this._eventBus.subscribe((event) => {
+            const ev = event as MyEvent
+            if(ev.label == this.REGISTER_MENU_EVENT_LABEL ) {
+                this.registerMenuAction(ev.payload)
+            }
+            if(ev.label == this.UNREGISTER_MENU_EVENT_LABEL ) {
+                this.unregisterMenuAction(ev.payload)
+            }
+        })
+    }
+
+    get currentMenuItems(): MenuItem[] {
+        return this.menuItems.getValue()
+    }
+
+    
+    registerMenu(menuItem:MenuItem) {
+        this._eventBus?.next({
+            label: this.REGISTER_MENU_EVENT_LABEL,
+            payload: {...menuItem}
+        })
+    }
+    
+    unregisterMenu(menuItem:MenuItem) {
+        this._eventBus?.next({
+            label: this.UNREGISTER_MENU_EVENT_LABEL,
+            payload: menuItem
+        })
+    }
+
+    private registerMenuAction(menuItem:MenuItem) {
+        this.menuItems.next([...this.currentMenuItems, menuItem])
+
+    }
+    private unregisterMenuAction(menuItem:MenuItem) {
+        this.menuItems.next(
+            this.currentMenuItems.filter( m => m.microfrontend !== menuItem.microfrontend || m.name != menuItem.name)
+        )
+    }
+    
+}
+
+export function createMenuApi(eventBus:ReplaySubject<unknown>) {
+    return new OrchyMenuApi(eventBus)
+}
+
